b1c9e1c2bfb68e140e43767ef0149b039a31a84a
Nick Tzaperas <nick@nijotz.com>
2025-06-10 13:51:16 -0700

    Add a Three.js scene to the home page

---
+++ src/app/components/Scene.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+function RotatingCube() {
+  const meshRef = useRef<Mesh>(null);
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta * 0.5;
+      meshRef.current.rotation.y += delta * 0.2;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#4f46e5" />
+    </mesh>
+  );
+}
+
+export default function Scene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <RotatingCube />
+    </Canvas>
+  );
+}
