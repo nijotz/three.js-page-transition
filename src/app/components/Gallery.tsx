c46a2ca6c83a12121ca090772ab0a773c93be599
Nick Tzaperas <nick@nijotz.com>
2025-06-17 20:43:55 -0700

    Wrap home gallery cubes so they don't overflow

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -22,7 +22,7 @@ export default function Gallery() {
       <div className="text-left p-8">
         <h1 className="text-4xl font-bold text-gray-900">Home</h1>
       </div>
-      <div className="flex">
+      <div className="flex flex-wrap justify-center">
         {cubes.map((cube: Cube) =>
           <CubeColor
             key={cube.id}
